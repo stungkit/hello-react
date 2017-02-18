7626f772e259ce1d8668e73f607d305433fb5aa1
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-02-18 10:32:46 -0800

    Finished Component State section.

--- public/app.jsx
+++ public/app.jsx
@@ -1,25 +1,47 @@
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
-      name: "React",
-      message: "This is from the component!"
+      name: 'React',
+      message: 'This is from the component'
     };
   },
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });  
+    }
+  },
   render: function() {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>{message}</p>
+        <p>{message + '!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
 });
 
-var firstName = "Smati";
-var message = "Tungkitkancharoen"
+var firstName = 'Smati';
 ReactDOM.render(
-  <Greeter name={firstName} message={message}/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
